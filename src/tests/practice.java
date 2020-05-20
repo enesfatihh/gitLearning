5d5ec7f2769494a569de01ff836aef8ff3bf339f
enesfatihh <enesfatih2001@hotmail.com>
2020-05-20 16:52:52 +0300

    asdasd

--- src/tests/practice.java
+++ src/tests/practice.java
@@ -1,9 +1,10 @@
 package tests;
 
 public class practice {
-
+    // asbsadasd
     //written on intellij
+    //dsadsasadsda
     //written on github
     //try fetch and merge
-
+    //asdasdasd
 }
