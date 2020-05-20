0249a5816577dc75b619b8018d4069739c833052
enesfatihh <61912441+enesfatihh@users.noreply.github.com>
2020-05-20 16:54:48 +0300

    Update practice.java

--- src/tests/practice.java
+++ src/tests/practice.java
@@ -1,11 +1,10 @@
 package tests;
 
 public class practice {
-    // asbsadasd
     //written on intellij
-    //dsadsasadsda
+    
     //written on github
-    //dsadsaas
+    
     //try fetch and merge
-    //asdasdasd
+ 
 }
